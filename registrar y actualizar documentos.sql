d003664b4fc2065dbb23af91ac1df319f5e0b898
descent2oblivion <58195806+descent2oblivion@users.noreply.github.com>
2020-02-20 16:16:02 -0700

    las bases de datos

---
+++ registrar y actualizar documentos.sql
@@ -0,0 +1,11 @@
+--[dbo].[regDoc] '004', 'reporte 6', 'Dina Michelle', 'Creacion del reporte 6', 'Emmanuel C.', 'Quebalix 1 - MTTO', 'Formatos'
+
+--[dbo].[actDoc] 4, '002', 'reporte', 'Dina Michelle', 'Creacion del reporte', 'Emmanuel Chavez', 'Quebalix 1 - MTTO', 'Formatos'
+go
+[dbo].[verDoc] 5
+go
+[dbo].[verRevs] 5
+go
+[dbo].[verHist] 5
+go
+[dbo].[verDoc] 5
